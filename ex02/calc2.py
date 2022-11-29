08d2fde8ddb4e8cead00f78e3665bbbb32649469
HOLLY <c0a2110979@edu.teu.ac.jp>
2022-11-29 17:08:13 +0900

    2

--- ex02/calc2.py
+++ ex02/calc2.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def button_click(ev):
+def button_click(ev):#数字を入力する
     btn = ev.widget
     txt = btn["text"]
     entry.insert(tk.END, F"{txt}")  
 
-def ap(ev):
+def ap(ev):#四則演算を入力する
     check = entry.get()
-    checker = check(-1)
     btn = ev.widget
     txt = btn["text"]
     """"
@@ -20,12 +19,12 @@ def ap(ev):
     entry.insert(tk.END, F"{txt}")
 
 
-def equal(ev):
+def equal(ev):#答えを出す！
     fm = entry.get()
     entry.delete(0, tk.END)
     entry.insert(tk.END, F"{eval(fm)}")
 
-
+#windowの大きさを設定
 root = tk.Tk()
 root.title("計算機の見た目")
 root.geometry("400x600")
@@ -34,7 +33,7 @@ root.geometry("400x600")
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
 entry.grid(row=0, column=0, columnspan=4)
 
-for i in range(10):
+for i in range(10):#数字ボタンを設置
     button = str(i)
     button = tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
     button.bind("<1>", button_click)
@@ -45,20 +44,20 @@ for i in range(10):
 
 count = 1
 ks = ["/", "*","-","+"]
-for i in ks:
+for i in ks:#四則演算の記号を設置
     count += 1
     button_ks = i
     button_ks = tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
     button_ks.bind("<1>", ap)
     button_ks.grid(row=count, column=3)
 
-#.
+#.を設置
 button_dot = "."
 button_dot = tk.Button(root, text=".", width=4, height=2, font=("", 30))
 button_dot.bind("<1>", ap)
 button_dot.grid(row=6, column=2)
 
-#=
+#=を設置
 button_equal = "="
 button_equal = tk.Button(root, text="=", width=4, height=2, font=("", 30))
 button_equal.bind("<1>", equal)
