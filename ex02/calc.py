55cef2f63a96e6649d6239c76b7157a3eeabad12
HOLLY <c0a2110979@edu.teu.ac.jp>
2022-11-29 14:50:28 +0900

    練習２

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+#練習１
 root = tk.Tk()
 root.title("計算機の見た目")
 root.geometry("300x500")
 
-def button_click(ev):
-    btn = ev.widget
-    txt = btn
+#練習2
+for i in range(0, 10):
+    button = tk.Button(root, text=f"{i}", width=4, height=2, font=("", 30))
+    button.grid(row=(9-i)//3, column=(9-i)%3)
+
+root.mainloop()
