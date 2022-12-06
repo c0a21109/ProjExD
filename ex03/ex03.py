543961a3363d9ea700e266efa1859303a8b63f31
HOLLY <c0a2110979@edu.teu.ac.jp>
2022-12-06 14:30:02 +0900

    ex

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -1,23 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def key_down(event):
-    global key
-    key = event.keysym
-
 if __name__ == "__main__":
-
-    cx = 300
-    cy = 400
-    key = ""
-    
     root = tk.Tk()
     root.title("迷える工科とん")
-
-    #image = tk.PhotoImage(file = "fig/5.ping")
     canvas =tk.Canvas(root, width=1500, height=900, bg=("black"))
-    canvas.create_image(cx, cy, image=image)
+    cx, cy = 300, 400
+    image=tk.PhotoImage(file="fig/8.png")
+    canvas.create_image(cx, cy,
+                        image=image,
+                        tag = "kokaton") 
     canvas.pack()   
-    
-
     tk.mainloop()
